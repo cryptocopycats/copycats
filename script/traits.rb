7e85c30ee1ac37defe96da0a436cbc5cedd5581a
Gerald Bauer <gerald.bauer@gmail.com>
2018-03-04 11:52:56 +0100

    up traits

--- script/traits.rb
+++ script/traits.rb
@@ -25,20 +25,77 @@ TRAITS_META = {
 }
 
 
+def make_rows( items, columns: 4 )
+  offset = items.size / columns
+  pp offset
+
+  rows = []
+  offset.times.with_index do |row|
+    ## note: construct [items[row],items[offset+row],items[offset*2+row], ...]
+    rows << columns.times.with_index.map { |col| items[offset*col+row] }
+  end
+  rows
+end
+
+def make_table( items )
+  rows = make_rows( items, columns: 4 )
+  pp rows
+
+  buf = ""
+  buf << "|Kai|Cattribute   |Kai|Cattribute  |Kai|Cattribute  |Kai|Cattribute  |\n"
+  buf << "|---|-------------|---|------------|---|------------|---|------------|\n"
+
+  rows.each do |row|
+    buf << "| "
+    buf <<  row.map {|item| "#{item[0]} | #{item[1]}" }.join( " | " )
+    buf << " |\n"
+  end
+
+  buf
+end
+
+
+buf = ""
+buf << "# Traits\n\n"
+
+headings = []
+TRAITS.keys.each do |key|
+  trait_meta = TRAITS_META[key]
+
+  headings << "#{trait_meta[:name]} (#{trait_meta[:genes]})"
+end
+
+buf << headings.join( " • " )
+buf << "\n\n"
+
+
 ## pp TRAITS
 TRAITS.keys.each do |key|
 
   trait      = TRAITS[key]
   trait_meta = TRAITS_META[key]
 
-  puts "## #{trait_meta[:name]} (Genes #{trait_meta[:genes]})\n\n"
 
   puts "Kai	 Cattribute"
-  Base32::ALPHABET.each_char do |kai|
+  items = []
+  Kai::ALPHABET.each_char do |kai|
     value = trait[kai]
     value = '??'  if value.nil? || value.empty?
-    puts "#{kai} #{value}"
+    items << [kai, value]
   end
 
-  puts "\n\n"
+  items.each do |item|
+    puts "#{item[0]} #{item[1]}"
+  end
+
+  buf << "## #{trait_meta[:name]} (Genes #{trait_meta[:genes]})\n\n"
+  buf << make_table( items )
+  buf << "\n\n"
+end
+
+
+puts buf
+
+File.open( "./TRAITS.md", "w" ) do |f|
+  f.write buf
 end
