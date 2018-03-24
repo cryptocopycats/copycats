3fa13f8836dcfb29a1dd9260f7125bd7e36588f5
Gerald Bauer <gerald.bauer@gmail.com>
2018-03-24 21:53:58 +0100

    up fancies

---
+++ lib/copycats/fancies.rb
@@ -0,0 +1,53 @@
+# encoding: utf-8
+
+###
+#
+#  for latest updates on new fancies/exclusives see:
+#      https://updates.cryptokitties.co   (official latest updates/timeline)
+
+
+## todo: add formula: []
+
+FANCIES =
+{
+  bugcat:       { name: 'Bug Cat',  limit: 3,     date: '2017-11-23', exclusive: true, ids: [101,102,103] },
+  shipcat:      { name: 'Ship Cat', limit: 2_000, date: '2017-11-23'  },
+
+  genesis:      { name: 'Genesis', limit: 1,      date: '2017-11-24', exclusive: true, ids: [1],
+                  desc: '1st Kitty' },
+
+  ducat:        { name: 'Du Cat', limit: 10_000,  date: '2017-11-29' },
+
+  dracula:      { name: 'Dracula',  limit: 2_000, date: '2017-12-01' },
+
+  mistletoe:    { name: 'Mistletoe', limit: 2_000, date: '2017-12-09',
+                  desc: "XMas Kitty" },
+
+  santaclaws:   { name: 'Santa Claws', limit: 1_000, date: '2017-12-12',
+                  desc: 'Ho Ho Ho - Santa Claus Kitty' },
+
+  phuziqaat:    { name: 'Phu Ziqaat', limit: 1_000, date: '2017-12-31',
+                  desc: 'Alien Kitty' },
+
+  stiches:      { name: 'Stiches', limit: 500, date: '2018-01-10',
+                  desc: 'Zombie Kitty' },
+
+
+
+  berry:        { name: 'Berry',    limit: 200  },
+  cathena:      { name: 'Cathena',  limit: 1,  desc: '500 000th Kitty' },
+  dogcat:       { name: 'Dog Cat', name_cn: '汪星爷', limit: 88, desc: 'Year of the Dog (Greater China)' },
+  earnie:       { name: 'Earnie', limit: 500, desc: 'Earn.com - Golden Kitty Award (Product Hunt)' },
+  fortunecat:   { name: 'Fortune Cat', name_cn: '红包喵', limit: 888 },
+  goldendogcat:    { name: 'Golden Dog Cat', name_cn: '旺财汪', limit: 11 },
+  goldendragoncat: { name: 'Golden Dragon Cat', name_cn: '帝龙喵', limit: 1, },
+  knightkitty:     { name: 'Knight Kitty', limit: 11 },
+  liondance:       { name: 'Lion Dance', name_cn: '咚咚锵', limit: 888 },
+  misterpurrfect:  { name: 'Mister Purrfect', limit: 1_000, desc: "Valentine's Day" },
+  'momo-chan':     { name: 'Momo-chan', limit: 500, desc: 'Ninja Kitty' },
+  negato:          { name: 'Negato',    limit: 500, desc: 'Ninja Kitty' },
+  pussforprogress: { name: 'Puss For Progress', limit: 1_920, desc: "Women's Day" },
+  tabby:           { name: 'Tabby', limit: 250 },
+  yuricatsuki:     { name: 'Yuri Catsuki', limit: 250, desc: 'Figure Scating Kitty - Winter Olympics (Korea)' }
+
+}
