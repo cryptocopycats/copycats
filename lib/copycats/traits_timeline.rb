04e423fcee0aae7bf8903c7ef4a30c000f1265e5
Gerald Bauer <gerald.bauer@gmail.com>
2018-12-05 22:17:05 +0100

    Create traits_timeline.rb

---
+++ lib/copycats/traits_timeline.rb
@@ -0,0 +1,295 @@
+# encoding: utf-8
+
+
+TRAITS_TIMELINE =
+{
+  ## 2018
+  ## november
+  dune:           { date: '2018-11-21' },
+  secretgarden:   { date: '2018-11-21' },
+  floorislava:    { date: '2018-11-21' },
+  junglebook:     { date: '2018-11-21' },
+
+  meowgarine:     { date: '2018-11-16' },
+  cornflower:     { date: '2018-11-16' },
+  icicle:         { date: '2018-11-16' },
+  hotcocoa:       { date: '2018-11-16' },
+  firstblush:     { date: '2018-11-16' },
+
+  featherbrain:   { date: '2018-11-13' },
+  foghornpawhorn: { date: '2018-11-13' },
+  alicorn:        { date: '2018-11-13' },
+  wyrm:           { date: '2018-11-13' },
+
+  mittens:        { date: '2018-11-06' },
+  arcreactor:     { date: '2018-11-06' },
+  allyouneed:     { date: '2018-11-06' },
+  gyre:           { date: '2018-11-06' },
+  moonrise:       { date: '2018-11-06' },
+
+  ## october
+  Metime:      { date: '2018-10-26' },
+  Jacked:      { date: '2018-10-26' },
+  Prism:       { date: '2018-10-26' },
+
+  Isotope:     { date: '2018-10-23' },
+  Bridesmaid:   { date: '2018-10-23' },
+  Downbythebay: { date: '2018-10-23' },
+  Gemini:       { date: '2018-10-23' },
+  Kaleidoscope: { date: '2018-10-23' },
+
+  Thatsawrap: { date: '2018-10-21' },    ## todo: move prestige to fancies!!!
+
+  Duckduckcat: { date: '2018-10-19' },
+
+  Siberian: { date: '2018-10-17' },
+  Lynx: { date: '2018-10-17' },
+  Toyger:  { date: '2018-10-17' },
+  Burmilla:  { date: '2018-10-17' },
+  Liger:  { date: '2018-10-17' },
+
+  Moue: { date: '2018-10-10' },
+  Majestic: { date: '2018-10-10' },
+  Satiated: { date: '2018-10-10' },
+  Struck: { date: '2018-10-10' },
+  Delite: { date: '2018-10-10' },
+
+  Myparade: { date: '2018-10-03' },
+  Roadtogold: { date: '2018-10-03' },
+
+
+  ## september
+  Dreamboat: { date: '2018-09-21' },
+  Fallspice: { date: '2018-09-21' },
+  Mallowflower: { date: '2018-09-21' },
+  Hanauma: { date: '2018-09-21' },
+  Summerbonnet: { date: '2018-09-21' },
+
+  Prune: { date: '2018-09-19' },
+  Furball: { date: '2018-09-19' },
+
+  Firedup: { date: '2018-09-06' },
+  Hacker: { date: '2018-09-06' },
+  Drama: { date: '2018-09-06' },
+  Candyshoppe: { date: '2018-09-06' },
+
+  Asif: { date: '2018-09-05' },
+
+  ## august
+  Dragonwings: { date: '2018-08-29' },
+  Aflutter: { date: '2018-08-29' },
+  Ducky: { date: '2018-08-29' },
+
+  Chantilly: { date: '2018-08-23' },
+  Mekong: { date: '2018-08-23' },
+  Fox: { date: '2018-08-23' },
+  Lykoi: { date: '2018-08-23' },
+
+  Tundra: { date: '2018-08-17' },
+  Glacier: { date: '2018-08-17' },
+  Hyacinth: { date: '2018-08-17' },
+  Shamrock: { date: '2018-08-17' },
+
+  Inflatablepool: { date: '2018-08-08' },
+  Peppermint: { date: '2018-08-08' },
+  Ooze: { date: '2018-08-08' },
+  Cyborg: { date: '2018-08-08' },
+
+  Scorpius: { date: '2018-08-01' },
+  Splat:   { date: '2018-08-01' },
+  Vigilante:  { date: '2018-08-01' },
+  Avatar:   { date: '2018-08-01' },
+
+  ## july
+  Topoftheworld: { date: '2018-07-25' },
+  Confuzzled:  { date: '2018-07-25' },
+  Samwise: { date: '2018-07-25' },
+  Walrus: { date: '2018-07-25' },
+
+  Olive: { date: '2018-07-20' },
+  Pinefresh: { date: '2018-07-20' },
+  Oasis: { date: '2018-07-20' },
+  Dioscuri: { date: '2018-07-20' },
+
+  Juju: { date: '2018-07-18' },
+  Frozen: { date: '2018-07-18' },
+
+  Kalahari: { date: '2018-07-13' },
+  Atlantis: { date: '2018-07-13' },
+
+  Fangtastic: { date: '2018-07-06' },
+
+  ## june
+  Littlefoot: { date: '2018-06-29' },
+  Dragontail: { date: '2018-06-29' },
+
+  Padparadscha: { date: '2018-06-28' },
+  Rosequartz:   { date: '2018-06-28' },
+  Universe:     { date: '2018-06-28' },
+
+  Martian:   { date: '2018-06-22' },
+  Redvelvet: { date: '2018-06-22' },
+  Brownies:  { date: '2018-06-22' },
+
+  Wowza: { date: '2018-06-19' },
+  Tendertears: { date: '2018-06-19' },
+
+  Impish: { date: '2018-06-14' },
+
+  Finalfrontier:  { date: '2018-06-11' },
+
+  Cobalt:     { date: '2018-06-09' },
+  Cashewmilk: { date: '2018-06-09' },
+  Buttercup:  { date: '2018-06-09' },
+
+  Manul:  { date: '2018-06-05' },
+  Balinese: { date: '2018-06-05' },
+  Kurilian: { date: '2018-06-05' },
+
+  ## may
+  Unicorn:  { date: '2018-05-31' },
+  Flapflap: { date: '2018-05-31' },
+
+  Dahlia:    { date: '2018-05-29' },
+  Palejade:  { date: '2018-05-29' },
+  Autumnmoon: { date: '2018-05-29' },
+
+  Razzledazzle: { date: '2018-05-24' },
+  Highsociety:  { date: '2018-05-24' },
+  Rorschach:    { date: '2018-05-24' },
+
+  Tinybox: { date: '2018-05-19' },
+
+  Butterscotch: { date: '2018-05-10' },
+  Garnet:  { date: '2018-05-10' },
+  Mertail: { date: '2018-05-10' },
+  Pearl:   { date: '2018-05-10' },
+
+  Highlander:  { date: '2018-05-08' },
+  Koladiviya:  { date: '2018-05-08' },
+
+  Swarley: { date: '2018-05-04' },
+  Oceanid: { date: '2018-05-04' },
+  Chameleon: { date: '2018-05-04' },
+  Bornwithit: { date: '2018-05-04' },
+
+  ## april
+  Cinderella: { date: '2018-04-27' },
+  Lavender:   { date: '2018-04-27' },
+
+  Daemonhorns: { date: '2018-04-24' },
+  Salty:       { date: '2018-04-24' },
+
+  Shale: { date: '2018-04-19' },
+
+  Eclipse:  { date: '2018-04-17' },
+  Parakeet: { date: '2018-04-17' },
+
+  Daemonwings:   { date: '2018-04-13' },
+
+  Caffeine:      { date: '2018-04-10' },
+
+  Frosting:         { date: '2018-04-02' },
+  Patrickstarfish:  { date: '2018-04-02' },
+  Mintmacaron:      { date: '2018-04-02' },
+
+
+  ## march
+  Doridnudibranch:  { date: '2018-03-30' },
+
+  Springcrocus:     { date: '2018-03-20' },
+
+  Thunderstruck:    { date: '2018-03-16' },
+  Rascal:           { date: '2018-03-16' },
+
+  Dragonfruit:  { date: '2018-03-14' },
+
+  Belch:  { date: '2018-03-09' },
+
+  Pixiebob:    { date: '2018-03-08' },
+  Poisonberry: { date: '2018-03-08' },
+  Safetyvest:  { date: '2018-03-08' },
+
+  Cyan:  { date: '2018-03-05' },
+
+
+  ## february
+  Missmuffet: { date: '2018-02-28' },
+  Wiley:      { date: '2018-02-28' },
+
+  Dippedcone:   { date: '2018-02-23' },
+  Leopard:      { date: '2018-02-23' },
+
+  Harbourfog:   { date: '2018-02-20' },
+
+  Baddate:      { date: '2018-02-13' },
+
+  Wuvme:  { date: '2018-02-12' },
+  Yokel:  { date: '2018-02-12' },
+  Starstruck:  { date: '2018-02-12' },
+
+  Egyptiankohl: { date: '2018-02-09' },
+  Bobtail: { date: '2018-02-09' },
+
+  Tiger: { date: '2018-02-06' },
+
+  Birman: { date: '2018-02-02' },
+  Coralsunrise: { date: '2018-02-02' },
+
+  ## january
+  Forgetmenot: { date: '2018-01-30' },
+  Savannah: { date: '2018-01-30' },
+  Norwegianforest: { date: '2018-01-30' },
+  Twilightsparkle: { date: '2018-01-30' },
+
+  Trioculus: { date: '2018-01-26' },
+
+  Morningglory: { date: '2018-01-20' },
+  Apricot:      { date: '2018-01-20' },
+  Turtleback:   { date: '2018-01-20' },
+  Wasntme:      { date: '2018-01-20' },
+  Cheeky:       { date: '2018-01-20' },
+
+  Sapphire:     { date: '2018-01-17' },
+
+  Nachocheez:  { date: '2018-01-14' },
+  Koala:       { date: '2018-01-14' },
+  Chronic:     { date: '2018-01-14' },
+  Onyx:        { date: '2018-01-14' },
+  Sass:        { date: '2018-01-14' },
+
+  Slyboots:   { date: '2018-01-13' },
+
+  Azaleablush:   { date: '2018-01-10' },
+  Spangled:      { date: '2018-01-10' },
+
+  Lilac:         { date: '2018-01-09' },
+  Bananacream:   { date: '2018-01-09' },
+  Verdigris:     { date: '2018-01-09' },
+
+  Neckbeard:    { date: '2018-01-06' },
+  Grim:         { date: '2018-01-06' },
+  Grimace:      { date: '2018-01-06' },
+  Stunned:      { date: '2018-01-06' },
+
+  Wonky:        { date: '2018-01-04' },
+  Babypuke:     { date: '2018-01-04' },
+  Pumpkin:      { date: '2018-01-04' },
+
+  Thundergrey:   { date: '2018-01-03' },
+
+
+  ## 2017
+  ## december
+  Selkirk:       { date: '2017-12-31' },
+  Icy:           { date: '2017-12-31' },
+  Flamingo:      { date: '2017-12-31' },
+  Seafoam:       { date: '2017-12-31' },
+
+  Elk:           { date: '2017-12-28' },
+
+  Hintomint:      { date: '2017-12-22' },
+
+  Serpent:        { date: '2017-12-20' },
+  Alien:          { date: '2017-12-20' }
+}
